93312eddbe7da3159613a73c6e5192b5a0b687c8
Arseniy <plakhotniuk.av@phystech.edu>
2021-11-01 18:30:21 +0300

    Pandas

--- Lab_1_MatplotLib/Episode3/Episode3.py
+++ Lab_1_MatplotLib/Episode3/Episode3.py
@@ -3,21 +3,20 @@ import matplotlib.pyplot as plt
 
 fig1, axes = plt.subplots(nrows = 4, ncols = 2)
 
-data = pd.read_csv('students.csv', sep='[;,|_]', engine='python')
+data = pd.read_csv('students.csv', sep='[;,|_]', engine='python', header=None)
 
 data.columns = ['Preps', 'Groups', 'Marks']
 
 j, k = 0, 0
 for i in data['Preps'].unique():
-    new_data = data.loc[data.Preps.isin([i])]['Marks'].value_counts().sort_index().to_frame()
-    print(new_data)
+    new_data = data.loc[data['Preps'] == i]['Marks'].value_counts().sort_index()
     new_data.plot.pie(y='Marks', figsize=(8, 8), ax=axes[j, k], autopct='%1.0f%%', shadow=True,
                       startangle=90, legend=False, fontsize=8, title=i)
     axes[j, k].title.set_size(10)
     if not k:
-        k+=1
+        k += 1
     else:
-        j +=1
+        j += 1
         k = 0
 
 fig1.delaxes(axes[3][1])
