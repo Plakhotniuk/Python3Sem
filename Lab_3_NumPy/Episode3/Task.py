7ed590efb5077076e819a05fe46175ace5eeb45c
Arseniy <plakhotniuk.av@phystech.edu>
2021-11-11 14:40:36 +0300

    Episode 3

---
+++ Lab_3_NumPy/Episode3/Task.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+data = np.loadtxt('start.dat')
+A = np.zeros([data.shape[0], data.shape[0]])
+np.fill_diagonal(A, 1)
+A[np.arange(data.shape[0]), np.arange(data.shape[0])-1] = -1
+fig = plt.figure()
+ax = plt.axes(xlim=(-1, 51), ylim=(data.min(), data.max()))
+line, = ax.plot([], [])
+plt.grid()
+
+
+def init():
+    line.set_data([], [])
+    return line,
+
+
+def animate(i):
+    global data
+    plt.title('Process time step: ' + str(i))
+    if not i:
+        line.set_data(np.linspace(0, 50, 50), data)
+    else:
+        data -= 0.5 * A.dot(data)
+        line.set_data(np.linspace(0, 50, 50), data)
+    return line,
+
+
+anim = FuncAnimation(fig, animate, init_func=init, frames=255, interval=70, blit=True)
+anim.save('graphics.gif', writer='pillow')
