c0dc6f887a0a2d0a66fdcce369b8efc111d57807
Arseniy <plakhotniuk.av@phystech.edu>
2021-11-23 14:37:32 +0300

    Episode3

--- Lab_4_SciPy_ SymPy/Episode2/Episode2.py
+++ Lab_4_SciPy_ SymPy/Episode2/Episode2.py
@@ -1,12 +1,12 @@
 from scipy import linalg
 import numpy as np
 import matplotlib.pyplot as plt
-with open('large.txt') as file:
+with open('Episode2/small.txt') as file:
     data = np.array([[float(j) for j in i.split()] for i in file], dtype=object)
     A = np.array([i for i in data[1:data.size-1]])
     b = np.array([i for i in data[-1]])
     x = linalg.solve(A, b)
     plt.bar(np.arange(1, x.size + 1), x)
     plt.grid()
+    plt.savefig('Episode2/small.png')
     plt.show()
-    plt.savefig('large.png')
---
+++ Lab_4_SciPy_ SymPy/Episode3/Sympy.py
@@ -0,0 +1,34 @@
+import sympy as sp
+import scipy.integrate
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(8,8))
+x = sp.Symbol('x')
+y = sp.Function('y')(x)
+
+diff = sp.Eq(y.diff(x) + 2 * y, 0)
+eq = sp.dsolve(diff, ics={y.subs(x, 0): sp.sqrt(2)})
+x_1 = np.linspace(0, 10, 100)
+y_1 = [eq.evalf(subs={x: i}).args[1] for i in x_1]
+ax[0].plot(x_1, y_1, '--', label='Sympy')
+ax[0].grid()
+
+
+def problem(y, x):
+    dydx = -2 * y
+    return dydx
+
+
+y0 = np.sqrt(2)
+y_2 = scipy.integrate.odeint(problem, y0, x_1)
+ax[0].plot(x_1, y_2, label='Scipy')
+ax[0].set_title('Solution of Sympy and Scipy')
+ax[0].legend()
+y_3 = [abs(y_2[i] - y_2[i]) for i in range(len(y_1))]
+ax[1].plot(x_1, y_3)
+ax[1].set_title('Difference between Sympy and Scipy')
+ax[1].grid()
+plt.show()
+plt.savefig('graphics.png')
+
