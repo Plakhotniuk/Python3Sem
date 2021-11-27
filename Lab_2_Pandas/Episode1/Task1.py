d11d22e05cff1e3444b72851e3bb322390624f6c
Arseniy <plakhotniuk.av@phystech.edu>
2021-11-27 23:45:10 +0300

    Task1

--- Lab_2_Pandas/Episode1/Task1.py
+++ Lab_2_Pandas/Episode1/Task1.py
@@ -1,4 +1,4 @@
 import pandas as pd
 data = pd.read_csv('transactions.csv.webarchive', index_col=0)
 data.reset_index()
-[print(data.loc[data['STATUS'] == 'OK'].sort_values(by=['SUM'], ascending=False)['SUM'].iloc[i]) for i in range(3)]
+[print(i) for i in data.loc[data['STATUS'] == 'OK'].sort_values(by=['SUM'], ascending=False)['SUM'].iloc[0:3].values]
